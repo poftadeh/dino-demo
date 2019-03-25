a5ab53fc040ea2e5d4868e55a18354f231ad1fa4
Pouya Oftadeh <poftadeh2@gmail.com>
2019-03-25 07:55:26 +0000

    Add Cactus and Pterodactyl handling

--- dinoAI.js
+++ dinoAI.js
@@ -5,26 +5,57 @@ const scope = () => {
 
   class DinoAI {
     constructor(runner) {
-      this.jumpKeyEvent = new window.KeyboardEvent('keydown', { keyCode: 32, which: 32 });
+      this.jumpKeyEvent = new KeyboardEvent('keydown', { keyCode: 32, which: 32 });
       this.duckKeyEvent = new KeyboardEvent('keydown', { keyCode: 40, which: 40 });
-      this.runner = runner;
-      // this.horizon = runner.instance_.horizon;
+      this.runner = runner.instance_;
+      this.horizon = runner.instance_.horizon;
       this.jump = this.jump.bind(this);
-      this.run = this.jump.bind(this);
-      console.log('LOGG', this.jumpKeyEvent, this.duckKeyEvent);
+      this.run = this.run.bind(this);
     }
 
     run() {
-      console.log(this, this.jumpKeyEvent);
+      if (!this.runner.crashed && this.horizonHasObstacles()) {
+        switch (this.horizon.obstacles[0].typeConfig.type) {
+          case 'CACTUS_SMALL':
+          case 'CACTUS_LARGE':
+            this.handleCactus();
+            break;
+          case 'PTERODACTYL':
+            this.handlePterodactyl();
+          default:
+        }
+      }
+    }
+
+    handleCactus() {
+      // jump, but not if already jumping
+      if (this.horizon.obstacles[0].xPos <= 100) {
+        this.jump();
+      }
+    }
+
+    handlePterodactyl() {
+      // bird is high
+      if (this.horizon.obstacles[0].yPos === 100 && this.horizon.obstacles[0].xPos <= 100) {
+        this.jump();
+        // bird is eye-level
+      } else if (this.horizon.obstacles[0].yPos === 75 && this.horizon.obstacles[0].xPos <= 100) {
+        this.duck();
+      }
     }
 
     jump() {
-      console.log('injump', this.jumpKeyEvent);
-      document.dispatchEvent(this.jumpKeyEvent);
+      console.log('injump');
+      if (!this.runner.tRex.midair) {
+        document.dispatchEvent(this.jumpKeyEvent);
+      }
     }
 
     duck() {
-      document.dispatchEvent(this.duckKeyEvent);
+      console.log('induck');
+      if (!this.runner.tRex.ducking) {
+        document.dispatchEvent(this.duckKeyEvent);
+      }
     }
 
     horizonHasObstacles() {
@@ -36,11 +67,9 @@ const scope = () => {
     }
   }
 
-  const dino = new DinoAI();
-  dino.run();
-  dino.jump();
+  const dino = new DinoAI(Runner);
 
-  setInterval(dino.jump, 0);
+  setInterval(dino.run, 0);
 
   // const jumpthing = () => {
   //   var evt = new KeyboardEvent('keydown', { 'keyCode': 32, 'which': 32 });
