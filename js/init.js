24bcc00bfc60b640b0702f4a8d72984655007fe3
Pouya Oftadeh <poftadeh2@gmail.com>
2019-04-03 16:50:57 -0400

    Remove tabIndex from init

--- js/init.js
+++ js/init.js
@@ -1,6 +1,5 @@
 document.body.classList.add('offline');
 new Runner('.interstitial-wrapper');
-document.querySelector('canvas').tabIndex= 2;
 const dino = new DinoAI(Runner);
 // dino.duck();
 dino.jump();
